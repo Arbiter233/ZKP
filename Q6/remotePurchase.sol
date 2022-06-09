cb990f2c434fac739602da1128017c2443faee3a
Arbiter <arbiter.huangmingyuan@hotmail.com>
2022-06-09 00:22:27 -0400

    ZKU pre

---
+++ Q6/remotePurchase.sol
@@ -0,0 +1,174 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+contract Purchase {
+    uint public value;
+    address payable public seller;
+    address payable public buyer;
+    uint buyerConfirmationTime; // add record of time when buyer comfirms the purchase
+
+    enum State { Created, Locked, Release, Inactive ,Complete}
+    // The state variable has a default value of the first member, `State.created`
+    State public state ;
+
+    
+    modifier condition(bool condition_) {
+        require(condition_);
+        _;
+    }
+
+    /// Only the buyer can call this function.
+    error OnlyBuyer();
+    /// Only the seller can call this function.
+    error OnlySeller();
+    /// The function cannot be called at the current state.
+    error InvalidState();
+    /// The provided value has to be even.
+    error ValueNotEven();
+
+    modifier onlyBuyer() {
+        if (msg.sender != buyer)
+            revert OnlyBuyer();
+        _;
+    }
+
+    modifier onlySeller() {
+        if (msg.sender != seller)
+            revert OnlySeller();
+        _;
+    }
+
+    modifier inState(State state_) {
+        if (state != state_)
+            revert InvalidState();
+        _;
+    }
+
+    event Aborted();
+    event PurchaseConfirmed();
+    event ItemReceived();
+    event SellerRefunded();
+
+    // Ensure that `msg.value` is an even number.
+    // Division will truncate if it is an odd number.
+    // Check via multiplication that it wasn't an odd number.
+    constructor() payable {
+        seller = payable(msg.sender);
+        value = msg.value / 2;
+        if ((2 * value) != msg.value)
+            revert ValueNotEven();
+        state = State.Created; //initialize state
+    }
+
+    /// Abort the purchase and reclaim the ether.
+    /// Can only be called by the seller before
+    /// the contract is locked.
+    function abort()
+        external
+        onlySeller
+        inState(State.Created)
+    {
+        emit Aborted();
+        state = State.Inactive;
+        // We use transfer here directly. It is
+        // reentrancy-safe, because it is the
+        // last call in this function and we
+        // already changed the state.
+        seller.transfer(address(this).balance);
+    }
+
+    /// Confirm the purchase as buyer.
+    /// Transaction has to include `2 * value` ether.
+    /// The ether will be locked until confirmReceived
+    /// is called.
+    
+    
+    
+    function confirmPurchase() 
+        external
+        inState(State.Created)
+        condition(msg.value == (2 * value))
+        payable
+        
+    {
+        emit PurchaseConfirmed();
+        buyer = payable(msg.sender);
+        state = State.Locked;
+        buyerConfirmationTime  = block.timestamp; // reconrd time when buyer confrims the purchase
+        
+    }
+
+    /// Confirm that you (the buyer) received the item.
+    /// This will release the locked ether.
+    /*function confirmReceived()
+        external
+        onlyBuyer
+        inState(State.Locked)
+    {
+        emit ItemReceived();
+        // It is important to change the state first because
+        // otherwise, the contracts called using `send` below
+        // can call in again here.
+        state = State.Release;
+
+        buyer.transfer(value);
+
+    }
+
+    /// This function refunds the seller, i.e.
+    /// pays back the locked funds of the seller.
+    function refundSeller()
+        external
+        onlySeller
+        inState(State.Release)
+    {
+        emit SellerRefunded();
+        // It is important to change the state first because
+        // otherwise, the contracts called using `send` below
+        // can call in again here.
+        state = State.Inactive;
+
+        seller.transfer(3 * value);
+    }
+    */
+
+    // testing modifier
+    modifier isBuyerOrTimePassed(){
+        bool con1 = (msg.sender == buyer);
+        bool con2 = block.timestamp >= (buyerConfirmationTime + 5 minutes);
+        if (con1 || con2){
+            _;
+        }else if (con1 || !con2){
+            revert timeNotPassed();
+        }else if (!con1 ||  con2){
+            revert OnlyBuyer();
+        }else{
+            revert ("both failed");
+        }
+            
+        
+        
+    }
+    // combination of confirmReceived and refundSeler
+
+    function completePurchase()
+        external
+        inState(State.Locked)
+        isBuyerOrTimePassed
+    {
+        emit ItemReceived();
+        
+        
+
+        buyer.transfer(value);
+        emit SellerRefunded();
+        
+        seller.transfer(3 * value);
+
+        state = State.Complete;
+    }
+    //for testing
+    function resetState() public {
+        state = State.Created;
+    }
+    error timeNotPassed(); //proof for working correctly
+}
